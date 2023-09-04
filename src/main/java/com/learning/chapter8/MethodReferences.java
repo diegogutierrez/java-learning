a7f46396d20353e7e49ba43c4995e676f1c38ffe
Diego Gutierrez <diego.guiterrez@lucidworks.com>
2023-09-03 19:52:19 -0500

    revert some changes

--- src/main/java/com/learning/chapter8/MethodReferences.java
+++ src/main/java/com/learning/chapter8/MethodReferences.java
@@ -1,5 +1,7 @@
 package com.learning.chapter8;
 
+import java.util.function.Function;
+
 public class MethodReferences {
 
   @FunctionalInterface
@@ -24,7 +26,15 @@ public class MethodReferences {
     StringCreator sc2 = MethodReferences::create;//static method
     StringCreatorClass instance = new MethodReferences().new StringCreatorClass();
     StringCreator sc3 = instance::create;//instance method
-    StringCreator sc4 = String::strip;//parameter method
+    StringCreator sc4 = String::strip;//parameter method. Strip signature: String strip();
     StringCreator sc5 = String::new;//constructor method
+
+    sc1.create("zxc");
+    sc2.create("zxc");
+    sc3.create("zxc");
+    sc4.create("zxc");
+    sc5.create("zxc");
+    Function<String, Integer> f = String::length;
+    f.apply("asd");
   }
 }
